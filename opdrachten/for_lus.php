e2333890b3cecf8dfbc7b0ab5f4b73185ed52b0d
Daswart <dswart@roc-dev.com>
2022-02-02 13:36:37 +0100

    Create for_lus.php

---
+++ opdrachten/for_lus.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voorbeld van de for-lus</title>
+</head>
+<body>
+    
+    <h3>Voorbeeld van de de for-lus</h3>
+    <?php
+    //Opgave 46 + 47
+    echo "We willen de for-lus 10 rondjes laten lopen<br>";
+    echo "Klaar voor de START...<br>";
+    for($teller = 5; $teller < 13; $teller++){
+        echo "Dit is rondje $teller <br>";
+
+    }
+    echo "FINISH...";
+
+    ?>
+</body>
+</html>
