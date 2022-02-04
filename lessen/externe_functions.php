85af829a158fee8da319f8def2c02f0a26d1712d
Daswart <dswart@roc-dev.com>
2022-02-04 08:35:17 +0100

    Commentaar toegevoegd

--- lessen/externe_functions.php
+++ lessen/externe_functions.php
@@ -28,6 +28,7 @@ function serviceKosten($betalingswijze){
 //Lab 1.15 stap 1
     function facturing(){
         
+        //Tonen gegevens in een tabel
         echo
         "<hr><h3>Factuur</h3> 
         <table>
@@ -42,6 +43,8 @@ function serviceKosten($betalingswijze){
 
         $bedrag = 0;
         $totaal = 0;
+
+        //Toon de gegevens per album, wanneer het aantal bestelde van elk album meer dan 0 is. 
         for($x=0; $x < sizeof($_POST['albumcode']); $x++){
             if($_POST['aantal'][$x] > 0){
             $bedrag = $_POST['prijs'][$x] * $_POST['aantal'][$x];
@@ -59,27 +62,29 @@ function serviceKosten($betalingswijze){
             
         }
         
+    //Totaalbedrag tonen.    
     echo "<tr>
     <td colspan='5'>Totaal</td>
     <td>".sprintf("%.2f", $totaal)."</td>
     </tr>";        
     
+    //De korting 
     $korting = 0;
     if(isset($_POST['student'])) $korting = $totaal * 0.15;  
     if(isset($_POST['klant'])) $korting = $totaal * 0.10;
-    
-    
     echo "<tr>
     <td colspan='5'>Korting</td>
     <td>".sprintf("%.2f", $korting)."</td>
     </tr>";
     
+    //De servicekosten
     $servicekosten = serviceKosten($_POST['betalingswijze']);
     echo "<tr>
     <td colspan='5'>Servicekosten</td>
     <td>".sprintf("%.2f", $servicekosten)."</td>
     </tr>";
-
+     
+    //Het te betalen bedrag.
     $totaal += $servicekosten - $korting; 
     echo "<tr>
     <td colspan='5'>Te betalen</td>
