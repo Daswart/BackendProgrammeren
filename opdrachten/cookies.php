0de8be06809ffd1cbe9cfbf97ec984c0e72f450e
Daswart <dswart@roc-dev.com>
2022-02-08 15:04:08 +0100

    Create cookies.php

---
+++ opdrachten/cookies.php
@@ -0,0 +1,10 @@
+<?php
+//Opgave 62
+setcookie("gebruiker", 'Sanskriet', time() + 3600);
+
+//Opgave 63
+echo "maak een cookie";
+setcookie('gebruiker', "Sanskriet", mktime(0,0,0,1,1,1,2050));
+
+
+?>
