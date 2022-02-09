4cff62d0523083fe4fb2608802faee172a3005ba
Daswart <dswart@roc-dev.com>
2022-02-09 16:06:36 +0100

    Update cookies.php

--- opdrachten/cookies.php
+++ opdrachten/cookies.php
@@ -3,8 +3,20 @@
 setcookie("gebruiker", 'Sanskriet', time() + 3600);
 
 //Opgave 63
-echo "maak een cookie";
-setcookie('gebruiker', "Sanskriet", mktime(0,0,0,1,1,1,2050));
+session_start();
+echo "Maak een cookie";
+setcookie('gebruiker', "Sanskriet", mktime(0,0,0,1,1,2050));
+$gebruiker = $_COOKIE['gebruiker'];
+echo "<br>Gebruikersnaam is: $gebruiker <br>";
+echo "<pre>";
+print_r($_COOKIE);
+echo "</pre>";
 
-
+//Opgave 64
+setcookie("user", "", time() + 3600);
+if(isset($_COOKIE['user'])){
+    echo $_COOKIE['user'];
+}else{
+    echo "<br>Cookie is verwijderd";
+}
 ?>
