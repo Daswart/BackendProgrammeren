4e6ab9c90e6c694f35e7a15058f9b2576fb1dea6
Daswart <dswart@roc-dev.com>
2022-02-15 14:02:17 +0100

    Update registreren.php

--- Blog/registreren.php
+++ Blog/registreren.php
@@ -4,23 +4,30 @@ if(isset($_POST['submit'])){
    $fotoNaam =($_FILES['foto']['name']);
    global $uploadsMap;
 
-   //functie upload file
+   //Functie upload file
    function upload(){
       global $uploadsMap;
       $uploadsMap = "uploads/". ($_FILES['foto']['name']); 
-      
+      $fototype = pathinfo($uploadsMap, PATHINFO_EXTENSION);
+
+      //Controleer of foto al bestaat
       if(file_exists($uploadsMap)){
-      echo "deze foto bestaat al";
+      echo "Deze foto bestaat al";
       return false;
       } 
       
+      //Valideer de maximale grootte van de afbeelding
       if($_FILES['foto']['size'] > 500000){
          echo "Deze foto is te groot";
          return false;
       }
+      //Valideer formaat
+      if($fototype != "jpg" && $fototype != "png" && $fototype != "jpeg" && $fototype != "gif"){
+         echo "Foto moet jpg, png, jpeg of gif zijn.";
+         return false;
+      }
       
       return true; 
-     
    }
 
    //Verplaats geüploade foto naar de uploadsmap wanneer foto uploaden is gelukt. 
@@ -43,11 +50,15 @@ if(isset($_POST['submit'])){
            }else{
               echo "Kon bestand niet afsluiten<br>";
            }
-      }
-      else{
+      }else{
          "Probleem bij het uploaden, Foto niet geüpload";
       }
    }
+   echo "<br>";
 }
 
 ?>
+
+<a href="registreren.html">
+   <input type="button" name="terug" value="Terug">
+</a>
