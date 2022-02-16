85dc169ca3b7c8ff90f17fccdeb9cfb2143a18ad
Daswart <dswart@roc-dev.com>
2022-02-16 18:01:54 +0100

    Update inloggen.php

--- Blog/inloggen.php
+++ Blog/inloggen.php
@@ -9,21 +9,24 @@ if(!$bestand){
 while(!feof($bestand)){
 $account = fgets($bestand);
 $account = explode("*", $account);
-if($account[1] == $email && $account[2] == $wachtwoord){
+    if($account[1] == $email && $account[2] == $wachtwoord){
     
     session_start();
     $_SESSION["USER"] = $email;
     $_SESSION["STATUS"] = 1;
-    $_SESSION["ID"] = $_COOKIE["PHPSESSID"];
+    $_SESSION["ID"] = session_id();
     echo "<script>alert('U bent ingelogd als $email');
     location.href='welkom.php';
     </script>";
     
+    }
 }
-}
-echo "<script>alert('Doei');
-    location.href='inloggen.html';
-    </script>";
+
+echo "<script>alert('Wachtwoord of gebruikersnaam ongeldig');
+location.href='inloggen.html';
+</script>";
+
+
 
 
 ?>
