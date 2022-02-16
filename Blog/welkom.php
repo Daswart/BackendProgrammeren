9152e430b640ed92c1d5b4b25384d6eb4af5d91f
Daswart <dswart@roc-dev.com>
2022-02-16 18:02:32 +0100

    Update welkom.php

--- Blog/welkom.php
+++ Blog/welkom.php
@@ -1,4 +1,14 @@
 <?php
-
-print_r($account);   
+session_start();
+if(isset($_SESSION['ID'])){
+    echo "<h3>Welkom</h3>";
+    echo '<a href="uitloggen.php"><input type="button" name="terug" value="Uitloggen"></a>';
+   
+}else{
+    echo "<br>Je moet eerst inloggen!<br><br>";
+    echo '<a href="inloggen.html"><input type="button" name="home" value="Naar inloggen"></a>';
+    setcookie('PHPSESSID', session_id(), time() -1, '/');
+}
 ?>
+
+
